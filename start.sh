80946cfa06b2c614e9e8c5cdf2b2b1d4fea1b435
Artem <hornymail@gmail.com>
2021-03-06 21:00:12 +0300

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+unlink ~/.tmux.conf
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+unlink ~/.vimrc 
+
 ln -s $PWD/.vimrc  ~/.vimrc
+unlink ~/.bash_aliases
+ln -s $PWD/.bash_aliases  ~/.bash_aliases
+unlink ~/.key-bindings.zsh
+ln -s $PWD/.key-bindings.zsh ~/.key-bindings.zsh
+
